395ed12afdec53ff3f0d335894e1306f24351966
alvian <alviandk@gmail.com>
2016-02-01 00:34:17 +0700

    setting staticfiles

---
+++ website/settings-deploy.py
@@ -0,0 +1,13 @@
+from settings import *
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['http://alviandk.com/','127.0.0.1']
+
+STATIC_ROOT='/home/alviandk/privat/static/'
+
+STATICFILES_DIRS = (    
+    '/home/alviandk/privat/private-upload/static/',
+)
--- website/settings.py
+++ website/settings.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT='/home/alvian/privat/upload/static/'
 STATIC_URL = '/static/'
 
 import os
@@ -116,9 +117,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),    
+    '/home/alvian/privat/upload/website/static/',
 )
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+
