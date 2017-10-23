42c46f75ad08e4bbfcb532678b1bcd50ebfe59e9
alviandk <alviandk@gmail.com>
2017-10-23 13:40:40 +0700

    update resume

--- website/settings.py
+++ website/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_ROOT='/home/alvian/privat/upload/static/'
-STATIC_URL = 'http://alviandk.com/static/'
+STATIC_URL = '/static/'
 
 
 import os
@@ -118,9 +118,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 STATICFILES_DIRS = (
-    
    
     '/home/alviandk/privat/private-upload/static/',
+    '/home/alvian/Github/private-upload/static/'
 )
 
 TEMPLATE_DIRS = (
